8fc830fccf662b0aab5c33db85210ea03e94e137
jc-alvarez-fdez <jc.alvarez.fdez@gmail.com>
2024-02-19 13:45:39 +0100

    @output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { UserComponent } from './components/user/user.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    UserComponent
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'angular17-tutorial';
+  userOcuppation: string = "Developer";
+
+  receiveEmision($event: any): void {
+    console.log($event)
+    throw new Error('Method not implemented.');
+    }
+
 }
---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,47 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+
+
+export class UserComponent {
+
+  @Input() occupation: string ="";
+  @Output() greet: EventEmitter<string> = new EventEmitter<string>();
+
+  username: string = "JC";
+
+  doesUserExists: boolean = true;
+  isEditable: boolean = true;
+
+  operatingSystems = [
+    {
+      id: 'win',
+      name: 'Windows'
+    },
+    {
+      id: 'osx',
+      name: 'MacOs'
+    },
+    {
+      id: 'linux',
+      name: 'Linux'
+    }
+  ];
+
+onMouseOver(osName: string): void {
+  console.log(osName);
+}
+
+emitToParent(): void {
+ this.greet.emit("Hi I'm your children")
+}
+
+
+
+}
